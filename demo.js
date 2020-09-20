bc39826ca4d427849fb4bbe14afae30eb6fd9ca0
amanpsharma <14mcaaman@gmail.com>
2020-09-19 23:10:15 -0700

    initial commit

---
+++ demo.js
@@ -0,0 +1,17 @@
+// // // var a=10;
+// // // var b=20;
+// // // console.log(a+b);
+
+// Using Function
+// // function test(){
+// //     console.log('Hello');
+// // }
+
+// // console.log(test());
+//-- Make url--
+var http = require('http');
+http.createServer(function (req,res){
+
+res.write('hello from node js server');
+res.end();
+}).listen(5000)
